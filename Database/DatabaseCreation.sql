dd90d07158b0d0cd366be13adac62277b2baae03
Nabajit Paul <149513573+NabajitPaul@users.noreply.github.com>
2025-12-09 00:16:29 +0530

    Create 'happytails' database and initial tables
    This SQL script creates the 'happytails' database and defines multiple tables including Addresses, Users, PetTypes, PetBreeds, Pets, Adoptables, AdoptionRequests, AdoptableImages, Messages, PetMedicalRecords, PetHealthStats, PetPreferences, CompabilityTags, and AdoptableCompability with their respective fields and constraints.

---
+++ Database/DatabaseCreation.sql
@@ -0,0 +1,153 @@
+CREATE DATABASE IF NOT EXISTS `happytails`;
+
+USE `happytails`;
+
+CREATE TABLE `happytails`.`Addresses` (
+	`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`house_no` VARCHAR(10),
+	`street` VARCHAR(30),
+	`landmark` VARCHAR(30),
+	`pincode` VARCHAR(30),
+	`town_city` VARCHAR(30),
+	`state` VARCHAR(30)
+);
+
+CREATE TABLE `happytails`.`Users` (
+	`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`name` VARCHAR(50),
+	`email` VARCHAR(100) NOT NULL,
+	`is_admin` ENUM('Yes', 'No') DEFAULT "No",
+	`password_hash` VARCHAR(100),
+	`roles` BINARY(2),
+	`address_id` INT UNSIGNED,
+	`profile_pic_name` VARCHAR(255),
+	`joined_on` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	CONSTRAINT `stays_at` FOREIGN KEY (`address_id`) REFERENCES `happytails`.`Addresses`(`id`)
+);
+
+CREATE TABLE `happytails`.`PetTypes` (
+	`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`name` VARCHAR(50)
+);
+
+CREATE TABLE `happytails`.`PetBreeds` (
+	`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`name` VARCHAR(50),
+	`type_id` INT UNSIGNED,
+	CONSTRAINT `is_f_brd` FOREIGN KEY (`type_id`) REFERENCES `happytails`.`PetTypes`(`id`)
+);
+
+CREATE TABLE `happytails`.`Pets` (
+	`pet_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`name` VARCHAR(50),
+	`gender` ENUM('Male', 'Female'),
+	`breed_id` INT UNSIGNED,
+	`type_id` INT UNSIGNED,
+	`dob` TIMESTAMP,
+	`de_wormed` ENUM('Yes', 'No', 'Unknown'),
+	`vaccinated` ENUM('Yes', 'No', 'Partially', 'Unknown'),
+	`last_vaccine_date` TIMESTAMP,
+	`sterilized` ENUM('Yes', 'No'),
+	`parent_id` INT UNSIGNED,
+	`pet_pic_name` VARCHAR(255),
+	`address_id` INT UNSIGNED,
+	CONSTRAINT `Owns` FOREIGN KEY (`parent_id`) REFERENCES `happytails`.`Users`(`id`),
+	CONSTRAINT `pt_brd` FOREIGN KEY (`breed_id`) REFERENCES `happytails`.`PetBreeds`(`id`),
+	CONSTRAINT `pt_typ` FOREIGN KEY (`type_id`) REFERENCES `happytails`.`PetTypes`(`id`),
+	CONSTRAINT `pet_stays_at` FOREIGN KEY (`address_id`) REFERENCES `happytails`.`Addresses`(`id`)
+);
+
+CREATE TABLE `happytails`.`Adoptables` (
+	`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`name` VARCHAR(50),
+	`caretaker_id` INT UNSIGNED,
+	`address_id` INT,
+	`breed_id` INT UNSIGNED,
+	`type_id` INT UNSIGNED,
+	`gender` ENUM('Male', 'Female'),
+	`dob` TIMESTAMP,
+	`weight_kg` DECIMAL(5,2),
+	`description` TEXT,
+	`sterilized` ENUM('Yes', 'No', 'Unknown'),
+	`vaccinated` ENUM('Yes', 'No', 'Partially', 'Unknown'),
+	`last_vaccine_date` TIMESTAMP,
+	`de_wormed` ENUM('Yes', 'No', 'Unknown'),
+	`house_trained` ENUM('Yes', 'No', 'In-Training'),
+	`status` ENUM('Available', 'Adopted', 'Hold'),
+	`added_on` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT `psts` FOREIGN KEY (`caretaker_id`) REFERENCES `happytails`.`Users`(`id`),
+	CONSTRAINT `adptbl_brd` FOREIGN KEY (`breed_id`) REFERENCES `happytails`.`PetBreeds`(`id`),
+	CONSTRAINT `catergorizes` FOREIGN KEY (`type_id`) REFERENCES `happytails`.`PetTypes`(`id`),
+	CONSTRAINT `adoptable_stays_at` FOREIGN KEY (`address_id`) REFERENCES `happytails`.`Addresses`(`id`)
+);
+
+CREATE TABLE `happytails`.`AdoptionRequests` (
+	`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`adoptable_id` INT UNSIGNED,
+	`adopter_id` INT UNSIGNED,
+	`status` ENUM('Pending', 'Interviewing', 'Approved', 'Rejected') DEFAULT "Pending",
+	`created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`message` TEXT,
+	CONSTRAINT `ar_rqstd` FOREIGN KEY (`adoptable_id`) REFERENCES `happytails`.`Adoptables`(`id`),
+	CONSTRAINT `makes_request` FOREIGN KEY (`adopter_id`) REFERENCES `happytails`.`Users`(`id`)
+);
+
+CREATE TABLE `happytails`.`AdoptableImages` (
+	`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`adoptable_id` INT UNSIGNED,
+	`filename` VARCHAR(255),
+	`is_primary` TINYINT,
+	`added_on` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT `has_media` FOREIGN KEY (`adoptable_id`) REFERENCES `happytails`.`Adoptables`(`id`)
+);
+
+CREATE TABLE `happytails`.`Messages` (
+	`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`adop_req_id` INT UNSIGNED,
+	`sent_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`sender_id` INT UNSIGNED,
+	`content` TEXT,
+	`is_read` TINYINT,
+	CONSTRAINT `sends_messages` FOREIGN KEY (`adop_req_id`) REFERENCES `happytails`.`AdoptionRequests`(`id`),
+	CONSTRAINT `snds` FOREIGN KEY (`sender_id`) REFERENCES `happytails`.`Users`(`id`)
+);
+
+CREATE TABLE `happytails`.`PetMedicalRecords` (
+	`id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`pet_id` INT UNSIGNED,
+	`title` VARCHAR(100),
+	`filename` VARCHAR(255) DEFAULT NULL,
+	`added_on` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT `has_medical_record` FOREIGN KEY (`pet_id`) REFERENCES `happytails`.`Pets`(`pet_id`)
+);
+
+CREATE TABLE `happytails`.`PetHealthStats` (
+	`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`pet_id` INT UNSIGNED,
+	`type` ENUM('Height', 'Weight'),
+	`value` DECIMAL(5,2) UNSIGNED DEFAULT 0,
+	`added_on` TIMESTAMP,
+	CONSTRAINT `hs_hlth_stt` FOREIGN KEY (`pet_id`) REFERENCES `happytails`.`Pets`(`pet_id`)
+);
+
+CREATE TABLE `happytails`.`PetPreferences` (
+	`id` INT UNSIGNED PRIMARY KEY NOT NULL,
+	`user_id` INT UNSIGNED,
+	`pref_name` VARCHAR(100),
+	`pref_value` VARCHAR(100),
+	CONSTRAINT `has` FOREIGN KEY (`user_id`) REFERENCES `happytails`.`Users`(`id`)
+);
+
+CREATE TABLE `happytails`.`CompabilityTags` (
+	`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`name` VARCHAR(50)
+);
+
+CREATE TABLE `happytails`.`AdoptableCompability` (
+	`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`adoptable_id` INT UNSIGNED,
+	`tag_id` INT UNSIGNED,
+	UNIQUE (`adoptable_id` ASC, `tag_id` ASC),
+	CONSTRAINT `fk_Adoptables_adoptable_id_to_AdoptableCompability_adoptable_id` FOREIGN KEY (`adoptable_id`) REFERENCES `happytails`.`Adoptables`(`id`),
+	CONSTRAINT `fk_CompabilityTags_tag_id_to_AdoptableCompability_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `happytails`.`CompabilityTags`(`id`)
+);
